b9a93446f842eac5c9c70a9992c2e6587871e872
Castor Mark Otieno <markcastorm@gmail.com>
2025-12-17 13:10:02 +0300

    updates

--- client/src/Pages/Services/Services.js
+++ client/src/Pages/Services/Services.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../../Components/Navbar'
 import Footer from '../../Components/Footer'
 import ServiceHero from './components/serviceHero'
+import ServicesCollection from './components/ServicesCollection'
 
 const Services = () => {
   return (
     <div>
       <Navbar/>
       <ServiceHero/>
+      <ServicesCollection/>
       <Footer/>
       
     </div>
